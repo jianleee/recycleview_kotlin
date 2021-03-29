039d577bf362542cc7bbe38a646bd5e61924e3eb
unknown <ECO0453_KIENLT@SMPC-020>
2021-03-29 10:36:52 +0700

    recycleview

---
+++ app/src/main/java/com/example/recycleview/ToDoAdadapter.kt
@@ -0,0 +1,28 @@
+package com.example.recycleview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_todo.view.*
+
+class ToDoAdadapter(
+    var todos: List<Todo>
+) : RecyclerView.Adapter<ToDoAdadapter.TodoViewHolder>() {
+    inner class TodoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_todo, parent, false)
+        return TodoViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return todos.size
+    }
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        holder.itemView.tvTitle.apply {
+            tvTitle.text = todos[position].title
+        }
+    }
+}
